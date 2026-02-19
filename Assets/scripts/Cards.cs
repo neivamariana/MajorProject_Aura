d9e9ed60aecac5a845b9f982d9316d153894b50e
neivamariana <neivasilva.mariana@gmail.com>
2026-02-19 00:08:03 +0000

    setup3

---
+++ Assets/scripts/Cards.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cards", menuName = "Scriptable Objects/Cards")]
+public class Cards : ScriptableObject
+{
+    
+}
---
+++ Assets/scripts/Deck.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Deck", menuName = "Scriptable Objects/Deck")]
+public class Deck : ScriptableObject
+{
+    
+}
