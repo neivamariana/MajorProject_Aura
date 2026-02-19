ea28382d767f5f17e12401eb44debe19e01f6177
neivamariana <neivasilva.mariana@gmail.com>
2026-02-19 11:02:21 +0000

    main menu

--- Assets/scripts/card_scripts/Cards.cs
+++ Assets/scripts/card_scripts/Cards.cs
@@ -3,5 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Cards", menuName = "Scriptable Objects/Cards")]
 public class Cards : ScriptableObject
 {
+    public string cardname;
+
+    public int cardNumber;
+
+    public string description;
+
+    public Sprite art;
     
 }
--- Assets/scripts/card_scripts/Deck.cs
+++ Assets/scripts/card_scripts/Deck.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using System.Collections.Generic;
+
 
 [CreateAssetMenu(fileName = "Deck", menuName = "Scriptable Objects/Deck")]
 public class Deck : ScriptableObject
 {
-    
+    public List<Cards> cards = new List<Cards>();
+
 }
